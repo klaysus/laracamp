30bbc9485b9e7072ff55dfc17fa4ae7791f59ac8
klaysus <kyorakukenichi31@gmail.com>
2022-04-21 23:37:38 +0700

    new commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use  App\Http\Controllers\User\CheckoutController;
+use App\Http\Controllers\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+
+// socialite routes
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google'); 
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
+
+
+Route::middleware(['auth'])->group(function(){
+    // checkout routes
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+
+    // user dashboard
+    Route::get('dashboard', [HomeController::class, 'dashboard'] )->name('dashboard');
+});
+
+require __DIR__.'/auth.php';
